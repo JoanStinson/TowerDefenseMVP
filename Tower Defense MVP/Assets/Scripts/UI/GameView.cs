76656ce9319a5a62816332e36e3baecaa180cc0e
Joan Ginard Mateo <jgm7.hd@gmail.com>
2026-01-10 22:42:54 +0100

    Popup variants + restart buttons

--- Tower Defense MVP/Assets/Scripts/UI/GameView.cs
+++ Tower Defense MVP/Assets/Scripts/UI/GameView.cs
@@ -2,20 +2,23 @@ using JGM.Gameplay;
 using JGM.Gameplay.Base;
 using JGM.Gameplay.Waves;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace JGM.UI
 {
     public class GameView : MonoBehaviour
     {
         [Header("Game Controller")]
-        [SerializeField]
-        private GameController gameController;
+        [SerializeField] private GameController gameController;
 
         [Header("Panels")]
         [SerializeField] private GameObject topPanel;
         [SerializeField] private GameObject bottomPanel;
-        [SerializeField] private GameObject losePopup;
-        [SerializeField] private GameObject winPopup;
+        [SerializeField] private GameObject popupLose;
+        [SerializeField] private GameObject popupWin;
+
+        [Header("Buttons")]
+        [SerializeField] private Button[] restartButtons;
 
         private void Awake()
         {
@@ -26,26 +29,38 @@ namespace JGM.UI
             gameController.OnGameStart += OnGameStart;
             playerBase.OnHealthDepleted += OnGameOver;
             wavesController.OnAllWavesComplete += OnGameWin;
+
+            foreach (var button in restartButtons)
+            {
+                button.onClick.AddListener(OnGameRestart);
+            }
         }
 
         private void OnGameStart()
         {
             topPanel.SetActive(true);
             bottomPanel.SetActive(true);
-            losePopup.SetActive(false);
-            winPopup.SetActive(false);
+            popupLose.SetActive(false);
+            popupWin.SetActive(false);
         }
 
         private void OnGameOver()
         {
-            losePopup.SetActive(true);
+            popupLose.SetActive(true);
             Time.timeScale = 0;
         }
 
         private void OnGameWin()
         {
-            winPopup.SetActive(true);
+            popupWin.SetActive(true);
             Time.timeScale = 0;
+        }
+
+        private void OnGameRestart()
+        {
+            popupLose.SetActive(false);
+            popupWin.SetActive(false);
+            Time.timeScale = 1;
         }
     }
 }

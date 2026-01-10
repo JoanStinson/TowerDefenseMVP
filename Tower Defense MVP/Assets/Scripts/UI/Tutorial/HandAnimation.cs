87d7976d361cfca855267e2ac146c756545115b6
Joan Ginard Mateo <jgm7.hd@gmail.com>
2026-01-10 22:11:43 +0100

    Added tutorial

---
+++ Tower Defense MVP/Assets/Scripts/UI/Tutorial/HandAnimation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace JGM.UI.Tutorial
+{
+    public class HandAnimation : MonoBehaviour
+    {
+        [SerializeField] private float distance = 70f;
+        [SerializeField] private float speed = 90f;
+
+        private Vector2 startPos;
+
+        private void Start()
+        {
+            startPos = transform.position;
+        }
+
+        private void Update()
+        {
+            PlayAnimation();
+        }
+
+        private void PlayAnimation()
+        {
+            float yOffset = Mathf.Repeat(Time.time * speed, distance);
+            transform.position = startPos + new Vector2(0, yOffset);
+        }
+    }
+}
---
+++ Tower Defense MVP/Assets/Scripts/UI/Tutorial/Tutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace JGM.UI.Tutorial
+{
+    public class Tutorial : MonoBehaviour
+    {
+        [SerializeField]
+        private float duration = 4f;
+
+        private void Start()
+        {
+            StartCoroutine(DisableAfterDelay());
+        }
+
+        private IEnumerator DisableAfterDelay()
+        {
+            yield return new WaitForSeconds(duration);
+            gameObject.SetActive(false);
+        }
+    }
+}
